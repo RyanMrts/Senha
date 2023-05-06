500ab6fc78a6f6afb30e8477a5f500cb60440548
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-05-05 22:40:07 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string resposta = "1234abcd";
+string senhadigitada;
+
+Console.Write("Opa opa, Insira sua senha para entrar: ");
+senhadigitada = Console.ReadLine()!;
+
+if (senhadigitada != resposta) {
+    Console.WriteLine("Senha errada, da próxima vez anote!");
+}
+if (senhadigitada == resposta) {
+    Console.WriteLine("Acesso liberado!");
+}
